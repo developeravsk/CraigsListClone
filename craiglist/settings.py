723242accc07c33c95b1176af91d8cb371e7ff32
developeravsk <readeravskh@gmail.com>
2022-01-04 11:09:14 +0545

    Static directory set

--- craiglist/settings.py
+++ craiglist/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'craiglist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIR=(os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
