6fdeb7dfc9d3cab960d971e6def7fe6125e2d177
developeravsk <readeravskh@gmail.com>
2022-01-04 15:07:02 +0545

    New search page created, base html code imported to new page

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns=[
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('new_search', views.new_search, name="new_search")
 ]
